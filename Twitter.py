e91d07c846a0520d2ad968d448f4e3ec59fcc9e3
James404 <68128416+James404-cyber@users.noreply.github.com>
2022-05-30 18:30:48 +0300

    Update Twitter.py

--- Twitter.py
+++ Twitter.py
@@ -6,8 +6,8 @@ try:os.system('touch .prox.txt')
 except:pass
 arc = str(platform.uname().machine)
 if 'arm' in arc:
-	__import__("Prohack")._site_ope_()
+	__import__("Twitter")._site_ope_()
 elif 'aarch' in arc:
-	__import__("Twitter")._site_view_()
+	__import__("Twitter64")._site_view_()
 else:
 	exit(f' Unknow device machine {arc}')
